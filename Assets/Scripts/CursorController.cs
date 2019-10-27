42a32f5f3d9e59e9ad690352d761465e7e7f053a
unknown <sean.kain.geo@gmail.com>
2019-10-27 11:17:36 -0400

    changing character input to be mouse based

---
+++ Assets/Scripts/CursorController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    [SerializeField]
+    private Texture2D cursorTexture;
+    [SerializeField]
+    private Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            player.GoTo(new Vector3(pos.x,pos.y,0));
+        }
+
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,11 +13,13 @@ public class Player : MonoBehaviour
     private float speed = 0;
     private Vector2 direction = Vector2.zero;
     private KeyCode currentKey = KeyCode.None;
+    private Vector3 destination;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        destination = transform.position;
     }
 
     private void ProcessKeyDown(KeyCode keyCode, int direction) {
@@ -27,63 +29,75 @@ public class Player : MonoBehaviour
         }
     }
 
+    public void GoTo(Vector3 location)
+    {
+        destination = location;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        var moveKeyDown = false;
-        
-        if (Input.GetKey(KeyCode.UpArrow))
+        //var moveKeyDown = false;
+        if (Vector3.Distance(transform.position, destination) > 0.3f)
         {
-            ProcessKeyDown(KeyCode.UpArrow, 2);
-            //anim.SetInteger("Direction", 2);
-            direction.x = 0;
-            direction.y = 1;
-            moveKeyDown = true;
+            this.transform.position = Vector3.MoveTowards(transform.position, destination, maxSpeed * Time.deltaTime);
+            anim.SetFloat("Speed", maxSpeed);
         }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            ProcessKeyDown(KeyCode.RightArrow, 1);
-            //anim.SetInteger("Direction", 1);
-            direction.x = 1;
-            direction.y = 0;
-            moveKeyDown = true;
+        else {
+            anim.SetFloat("Speed", 0);
+        }
+        //if (Input.GetKey(KeyCode.UpArrow))
+        //{
+        //    ProcessKeyDown(KeyCode.UpArrow, 2);
+        //    //anim.SetInteger("Direction", 2);
+        //    direction.x = 0;
+        //    direction.y = 1;
+        //    moveKeyDown = true;
+        //}
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    ProcessKeyDown(KeyCode.RightArrow, 1);
+        //    //anim.SetInteger("Direction", 1);
+        //    direction.x = 1;
+        //    direction.y = 0;
+        //    moveKeyDown = true;
             
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            ProcessKeyDown(KeyCode.LeftArrow, 3);
-            //anim.SetInteger("Direction", 3);
-            direction.x = -1;
-            direction.y = 0;
-            moveKeyDown = true;
+        //}
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    ProcessKeyDown(KeyCode.LeftArrow, 3);
+        //    //anim.SetInteger("Direction", 3);
+        //    direction.x = -1;
+        //    direction.y = 0;
+        //    moveKeyDown = true;
             
-        }
-        if ( Input.GetKey(KeyCode.DownArrow))
-        {
-            ProcessKeyDown(KeyCode.DownArrow, 0);
-            //anim.SetInteger("Direction", 0);
-            direction.x = 0;
-            direction.y = -1;
-            moveKeyDown = true;
-        }
-        if (moveKeyDown)
-        {
-            if (speed < maxSpeed)
-            {
-                speed += acceleration;
-            }
-        }
-        else
-        {
-            if (speed > 0)
-            {
-                speed -= acceleration;
-            }
-            currentKey = KeyCode.None;
-        }
+        //}
+        //if ( Input.GetKey(KeyCode.DownArrow))
+        //{
+        //    ProcessKeyDown(KeyCode.DownArrow, 0);
+        //    //anim.SetInteger("Direction", 0);
+        //    direction.x = 0;
+        //    direction.y = -1;
+        //    moveKeyDown = true;
+        //}
+        //if (moveKeyDown)
+        //{
+        //    if (speed < maxSpeed)
+        //    {
+        //        speed += acceleration;
+        //    }
+        //}
+        //else
+        //{
+        //    if (speed > 0)
+        //    {
+        //        speed -= acceleration;
+        //    }
+        //    currentKey = KeyCode.None;
+        //}
 
-        anim.SetBool("MoveKeyDown", moveKeyDown);
-        anim.SetFloat("Speed", speed);
-        this.transform.position += new Vector3(direction.x * speed * Time.deltaTime, direction.y * speed * Time.deltaTime, 0);
+        //anim.SetBool("MoveKeyDown", moveKeyDown);
+        //anim.SetFloat("Speed", speed);
+        //this.transform.position += new Vector3(direction.x * speed * Time.deltaTime, direction.y * speed * Time.deltaTime, 0);
     }
 }
