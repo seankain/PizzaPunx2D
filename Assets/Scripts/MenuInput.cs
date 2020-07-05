73a617ebc8eaaed1019b71849db3d0f6a9f38728
unknown <sean.kain.geo@gmail.com>
2020-07-04 22:35:04 -0400

    intro screen

---
+++ Assets/Scripts/MenuInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInput : MonoBehaviour
+{
+    public Scene Level;
+    public AudioSource selectionSound;
+    public AudioSource music;
+    private bool transitioning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            transitioning = true;
+            music.Stop();
+            selectionSound.Play();
+
+            //SceneManager.SetActiveScene(Level);
+        }
+        //Wait for selection sound to finish playing
+        if(transitioning && !selectionSound.isPlaying) { SceneManager.LoadScene(1); }
+    }
+}
