71e91a98f446a6397905e32b325952200a61a8e1
Blake Schreurs <dracolytch@yahoo.com>
2020-07-06 21:33:47 -0400

    Gripely animations

---
+++ Assets/Scripts/Gripely/ReviewData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReviewData
+{
+    public int reviewId;
+    public string reviewText;
+    public bool isGoodReview;
+}
